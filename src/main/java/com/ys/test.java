49857a84c3d762faf4e008fb25c5809fd5768d6b
lys <lys@233.com>
2021-05-02 13:15:51 +0800

    first commit

---
+++ src/main/java/com/ys/test.java
@@ -0,0 +1,11 @@
+package com.ys;
+
+/**
+ * @author lys
+ * @create 2021-05-02-13:09
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("ystest");
+    }
+}
